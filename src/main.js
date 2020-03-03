f7e13809a708d3c3bcd616cfe73cd059c345fc5b
Talila Burka <btalila@yahoo.com>
2020-03-03 17:46:57 -0600

    made a constructor for easy template

--- src/main.js
+++ src/main.js
@@ -10,3 +10,19 @@ const Manager = require('./manager');
 
 const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
+
+class Main {
+	constructor() {
+		this._teamArray = [];
+	}
+
+	async _easy() {
+		let teamHTMLString = '';
+		for (const teamMember of this._teamArray) {
+			teamHTMLString += teamMember.easy();
+		}
+
+		const result = Main._templateStart + teamHTMLString + Main._templateEnd;
+
+		await writeFileAsync(path.resolve(__dirname, '..', 'dist', 'easy.html'), result);
+	}
