1bdea048856fc7abdb794fa3d57a4afa9571ba3e
Talila Burka <btalila@yahoo.com>
2020-05-21 11:57:11 -0500

    Made changes to test files

--- test/employee.test.js
+++ test/employee.test.js
@@ -1,4 +1,4 @@
-const Employee = require("../src/employee");
+const Employee = require("../lib/Employee");
 
 test("Can instantiate Employee instance", () => {
   const e = new Employee();
@@ -8,19 +8,19 @@ test("Can instantiate Employee instance", () => {
 test("Can set name via constructor arguments", () => {
   const name = "Alice";
   const e = new Employee(name);
-  expect(_e.name).toBe(name);
+  expect(e.name).toBe(name);
 });
 
 test("Can set id via constructor argument", () => {
   const testValue = 100;
   const e = new Employee("Foo", testValue);
-  expect(_e.id).toBe(testValue);
+  expect(e.id).toBe(testValue);
 });
 
 test("Can set email via constructor argument", () => {
   const testValue = "test@test.com";
   const e = new Employee("Foo", 1, testValue);
-  expect(_e.email).toBe(testValue);
+  expect(e.email).toBe(testValue);
 });
 
 test("Can get name via getName()", () => {
--- test/engineer.test.js
+++ test/engineer.test.js
@@ -1,9 +1,9 @@
-const Engineer = require("../src/engineer");
+const Engineer = require("../lib/Engineer");
 
 test("Can set GitHUb account via constructor", () => {
   const testValue = "GitHubUser";
   const e = new Engineer("Foo", 1, "test@test.com", testValue);
-  expect(e._github).toBe(testValue);
+  expect(e.github).toBe(testValue);
 });
 
 test("getRole() should return \"Engineer\"", () => {
